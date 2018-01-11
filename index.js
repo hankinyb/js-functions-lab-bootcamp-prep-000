d1b8f601b82e2c5113fd53451a8efbfd28429566
Yan Hankin <hankinyb@gmail.com>
2018-01-11 05:19:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,3 +5,10 @@ function happyHolidays() {
 function happyHolidaysTo(name) {
   return `Happy holidays, ${name}!`
 }
+function happyHolidayTo(holday, name) {
+  return `Happy ${holiday}, ${name}!`
+}
+function holidayCountdown(days, holiday) {
+  return `It's ${days} days until ${holiday}!`
+}
+
